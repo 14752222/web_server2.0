3c6f531a6d0f0b5feb3e04f3952a04f428809035
while(1) <53716400+14752222@users.noreply.github.com>
2023-10-25 21:30:53 +0800

    新增自动增加版本

---
+++ config/version.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+ # 找到 ./config.int 里面的版本号 每次触发构建都会自动加1
+#!/bin/bash
+
+# 从配置文件中提取版本号
+version=$(grep "version =" ./config.ini | awk -F '=' '{print $2}' | tr -d ' ')
+
+# 将版本号拆分为主版本、次版本和修订版本
+IFS='.' read -ra parts <<< "$version"
+
+# 增加修订版本
+parts[2]=$((${parts[2]+1}))
+
+# 处理版本号溢出
+if [ "${parts[2]}" -eq 10 ]; then
+  parts[2]=0
+  parts[1]=$((${parts[1]+1}))
+fi
+
+if [ "${parts[1]}" -eq 10 ]; then
+  parts[1]=0
+  parts[0]=$((${parts[0]+1}))
+fi
+
+# 重新构建版本号
+new_version="${parts[0]}.${parts[1]}.${parts[2]}"
+
+
+# 将新版本号写入配置文件
+sed -i "s/version = $version/version = $new_version/g" ./config.ini
+
+# 输出版本号
+echo "Original Version: $version"
+echo "New Version: $new_version"
+
+
+
