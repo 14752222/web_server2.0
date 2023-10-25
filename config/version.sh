6795b24a0c822e88392e2b866e88938b927b343d
while(1) <53716400+14752222@users.noreply.github.com>
2023-10-25 21:53:12 +0800

    version.sh fix

--- config/version.sh
+++ config/version.sh
@@ -1,27 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # 从配置文件中提取版本号
 version=$(grep "version =" ./config/config.ini | awk -F '=' '{print $2}' | tr -d ' ')
 
 # 将版本号拆分为主版本、次版本和修订版本
-IFS='.' read -ra parts <<< "$version"
+IFS='.' read -r part1 part2 part3 <<EOF
+$version
+EOF
 
 # 增加修订版本
-parts[2]=$((${parts[2]+1}))
+part3=$((part3 + 1))
 
 # 处理版本号溢出
-if [ "${parts[2]}" -eq 10 ]; then
-  parts[2]=0
-  parts[1]=$((${parts[1]+1}))
+if [ $part3 -eq 10 ]; then
+  part3=0
+  part2=$((part2 + 1))
 fi
 
-if [ "${parts[1]}" -eq 10 ]; then
-  parts[1]=0
-  parts[0]=$((${parts[0]+1}))
+if [ $part2 -eq 10 ]; then
+  part2=0
+  part1=$((part1 + 1))
 fi
 
 # 重新构建版本号
-new_version="${parts[0]}.${parts[1]}.${parts[2]}"
+new_version="$part1.$part2.$part3"
 
 # 将新版本号写入配置文件
 sed -i "s/version = $version/version = $new_version/g" ./config.ini
@@ -29,6 +31,3 @@ sed -i "s/version = $version/version = $new_version/g" ./config.ini
 # 输出版本号
 echo "Original Version: $version"
 echo "New Version: $new_version"
-
-
-
