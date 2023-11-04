da34826f41d5b9ed7f1b5b09a517575be61bf550
while(1) <53716400+14752222@users.noreply.github.com>
2023-11-04 19:23:42 +0800

    workflow:工流优化

--- api/controller/controller.go
+++ api/controller/controller.go
@@ -4,6 +4,28 @@ import "github.com/gin-gonic/gin"
 
 type BaseController struct{}
 
+type base struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type Success struct {
+	base
+	Data interface{} `json:"data"`
+}
+
+type RequestError struct {
+	base
+	Data interface{} `json:"data"`
+}
+
+type PageList struct {
+	base
+	Total int         `json:"total"`
+	List  interface{} `json:"list"`
+	Size  int         `json:"size"`
+}
+
 func (b *BaseController) SendError(ctx *gin.Context, code int8, message string, data any) {
 	ctx.JSON(200, gin.H{
 		"code":    code,
@@ -18,7 +40,6 @@ func (b *BaseController) SendSuccess(ctx *gin.Context, code int8, message string
 		"data":    data,
 	})
 }
-
 func (b *BaseController) SendPage(ctx *gin.Context, code int8, message string, data any, total int64, page int64, limit int64) {
 	ctx.JSON(200, gin.H{
 		"code":    code,
